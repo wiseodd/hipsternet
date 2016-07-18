ab0ba2f3f9dfbb83425761a304f065a03786113a
Agustinus Kristiadi <kristiadi7@gmail.com>
2016-07-18 19:31:14 +0700

    Add avgpool

--- hipsternet/layer.py
+++ hipsternet/layer.py
@@ -156,6 +156,40 @@ def conv_backward(dout, cache):
 
 
 def maxpool_forward(X, size=2, stride=2):
+    def maxpool(X_col):
+        max_idx = np.argmax(X_col, axis=0)
+        out = X_col[max_idx, range(max_idx.size)]
+        return out, max_idx
+
+    return _pool_forward(X, maxpool, size, stride)
+
+
+def maxpool_backward(dout, cache):
+    def dmaxpool(dX_col, dout_col, pool_cache):
+        dX_col[pool_cache, range(dout_col.size)] = dout_col
+        return dX_col
+
+    return _pool_backward(dout, dmaxpool, cache)
+
+
+def avgpool_forward(X, size=2, stride=2):
+    def avgpool(X_col):
+        out = np.mean(X_col, axis=0)
+        cache = None
+        return out, cache
+
+    return _pool_forward(X, avgpool, size, stride)
+
+
+def avgpool_backward(dout, cache):
+    def davgpool(dX_col, dout_col, pool_cache):
+        dX_col[:, range(dout_col.size)] = 1. / dX_col.shape[0] * dout_col
+        return dX_col
+
+    return _pool_backward(dout, davgpool, cache)
+
+
+def _pool_forward(X, pool_fun, size=2, stride=2):
     n, d, h, w = X.shape
     h_out = (h - size) / stride + 1
     w_out = (w - size) / stride + 1
@@ -167,25 +201,26 @@ def maxpool_forward(X, size=2, stride=2):
 
     X_reshaped = X.reshape(n * d, 1, h, w)
     X_col = im2col_indices(X_reshaped, size, size, padding=0, stride=stride)
-    max_idx = np.argmax(X_col, axis=0)
 
-    out = X_col[max_idx, range(max_idx.size)]
+    out, pool_cache = pool_fun(X_col)
+
     out = out.reshape(h_out, w_out, n, d)
     out = out.transpose(2, 3, 0, 1)
 
-    cache = (X, size, stride, X_col, max_idx)
+    cache = (X, size, stride, X_col, pool_cache)
 
     return out, cache
 
 
-def maxpool_backward(dout, cache):
-    X, size, stride, X_col, max_idx = cache
+def _pool_backward(dout, dpool_fun, cache):
+    X, size, stride, X_col, pool_cache = cache
     n, d, w, h = X.shape
 
     dX_col = np.zeros_like(X_col)
-    dout_flat = dout.transpose(2, 3, 0, 1).ravel()
+    dout_col = dout.transpose(2, 3, 0, 1).ravel()
 
-    dX_col[max_idx, range(max_idx.size)] = dout_flat
+    dX = dpool_fun(dX_col, dout_col, pool_cache)
+
     dX = col2im_indices(dX_col, (n * d, 1, h, w), size, size, padding=0, stride=stride)
     dX = dX.reshape(X.shape)
 
