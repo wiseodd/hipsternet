91352af6303f25579aeb30017e1f382cd6ac8db7
Agustinus Kristiadi <kristiadi7@gmail.com>
2016-08-13 20:49:35 +0400

    WIP: Add GRU model

--- hipsternet/neuralnet.py
+++ hipsternet/neuralnet.py
@@ -486,3 +486,92 @@ class LSTM(RNN):
             bo=np.zeros((1, H)),
             by=np.zeros((1, D))
         )
+
+
+class GRU(RNN):
+
+    def __init__(self, D, H, char2idx, idx2char):
+        super().__init__(D, H, char2idx, idx2char)
+
+    def forward(self, X, h_old, train=True):
+        m = self.model
+        Wz, Wr, Wh, Wy = m['Wz'], m['Wr'], m['Wh'], m['Wy']
+        bz, br, bh, by = m['bz'], m['br'], m['bh'], m['by']
+
+        X_one_hot = np.zeros(self.D)
+        X_one_hot[X] = 1.
+        X_one_hot = X_one_hot.reshape(1, -1)
+
+        X = np.column_stack((h_old, X_one_hot))
+
+        hz, hz_cache = l.fc_forward(X, Wz, bz)
+        hz, hz_sigm_cache = l.sigmoid_forward(hz)
+
+        hr, hr_cache = l.fc_forward(X, Wr, br)
+        hr, hr_sigm_cache = l.sigmoid_forward(hr)
+
+        X_prime = np.column_stack((hr * h_old, X_one_hot))
+        hh, hh_cache = l.fc_forward(X_prime, Wh, bh)
+        hh, hh_tanh_cache = l.tanh_forward(hh)
+
+        h = (1. - hz) * h_old + hz * hh
+
+        y, y_cache = l.fc_forward(h, Wy, by)
+
+        cache = (
+            X, X_prime, h_old, hz, hz_cache, hz_sigm_cache, hr, hr_cache, hr_sigm_cache,
+            hh, hh_cache, hh_tanh_cache, h, y_cache
+        )
+
+        if not train:
+            y = util.softmax(y)
+
+        return y, h, cache
+
+    def backward(self, y_pred, y_train, dh_next, cache):
+        X, X_prime, h_old, hz, hz_cache, hz_sigm_cache, hr, hr_cache, hr_sigm_cache, hh, hh_cache, hh_tanh_cache, h, y_cache = cache
+
+        dy = loss_fun.dcross_entropy(y_pred, y_train)
+
+        dh, dWy, dby = l.fc_backward(dy, y_cache)
+        dh += dh_next
+
+        dhh = hz * dh
+        dh_old1 = (1 - hz) * dh
+        dhz = hh * dh - h_old * dh
+
+        dhh = l.tanh_backward(dhh, hh_tanh_cache)
+        dX_prime, dWh, dbh = l.fc_backward(dhh, hh_cache)
+
+        dh_prime = dX_prime[:, self.H]
+        dh_old1 = hr * dh_prime
+
+        dhr = h_old * dh_prime
+        dhr = l.sigmoid_backward(dhr, hr_sigm_cache)
+        dXr, dWr, dbr = l.fc_backward(dhr, hr_cache)
+
+        dhz = l.sigmoid_backward(dhz, hz_sigm_cache)
+        dXz, dWz, dbz = l.fc_backward(dhz, hz_cache)
+
+        dX = dXr + dXz
+        dh_old2 = dX[:, self.H]
+
+        dh_next = dh_old1 + dh_old2
+
+        grad = dict(Wz=dWz, Wr=dWr, Wh=dWh, Wy=dWy, bz=dbz, br=dbr, bh=dbh, by=dby)
+
+        return grad, dh_next
+
+    def _init_model(self, D, C, H):
+        Z = H + D
+
+        self.model = dict(
+            Wz=np.random.randn(Z, H) / np.sqrt(Z / 2.),
+            Wr=np.random.randn(Z, H) / np.sqrt(Z / 2.),
+            Wh=np.random.randn(Z, H) / np.sqrt(Z / 2.),
+            Wy=np.random.randn(H, D) / np.sqrt(D / 2.),
+            bz=np.zeros((1, H)),
+            br=np.zeros((1, H)),
+            bh=np.zeros((1, H)),
+            by=np.zeros((1, D))
+        )
--- run_rnn.py
+++ run_rnn.py
@@ -4,12 +4,12 @@ import hipsternet.neuralnet as nn
 import hipsternet.solver as solver
 
 
-time_step = 25
+time_step = 10
 n_iter = 1000000000
 alpha = 1e-3
 print_after = 100
 
-H = 128
+H = 64
 
 
 if __name__ == '__main__':
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 
     if len(sys.argv) > 1:
         net_type = sys.argv[1]
-        valid_nets = ('rnn', 'lstm')
+        valid_nets = ('rnn', 'lstm', 'gru')
 
         if net_type not in valid_nets:
             raise Exception('Valid network type are {}'.format(valid_nets))
@@ -42,6 +42,8 @@ if __name__ == '__main__':
         net = nn.LSTM(vocab_size, H=H, char2idx=char_to_idx, idx2char=idx_to_char)
     elif net_type == 'rnn':
         net = nn.RNN(vocab_size, H=H, char2idx=char_to_idx, idx2char=idx_to_char)
+    elif net_type == 'gru':
+        net = nn.GRU(vocab_size, H=H, char2idx=char_to_idx, idx2char=idx_to_char)
 
     solver.adam_rnn(
         net, X, y, net.initial_state(),
