4f0774850ec6dcc1381fb6e673c831783bfeb2e5
Agustinus Kristiadi <kristiadi7@gmail.com>
2016-12-06 09:18:17 +0700

    Fix contractive autoencoders

--- adhoc/autoencoders.py
+++ adhoc/autoencoders.py
@@ -2,12 +2,15 @@ from tensorflow.examples.tutorials.mnist import input_data
 from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Deconv2D, UpSampling2D, Flatten, Reshape
 from keras.models import Model
 from keras.optimizers import Adam
-from keras.regularizers import l1
+from keras.regularizers import activity_l1
 
 import numpy as np
 import matplotlib.pyplot as plt
 import keras.backend as K
 import tensorflow as tf
+
+
+tf.python.control_flow_ops = tf
 
 
 mnist = input_data.read_data_sets('../data/MNIST_data', one_hot=True)
@@ -29,30 +32,41 @@ def autoencoder(X, loss='l2', lam=0.):
     loss = 'mae' if loss == 'l1' else 'mse'
 
     model.compile(optimizer='adam', loss=loss)
-    model.fit(X, X, batch_size=64, nb_epoch=5)
+    model.fit(X, X, batch_size=64, nb_epoch=3)
 
-    return model
+    return model, Model(input=inputs, output=h)
 
 
-def sparse_autoencoder(X, lam=1e-3):
+def sparse_autoencoder(X, lam=1e-5):
     X = X.reshape(X.shape[0], -1)
     M, N = X.shape
 
     inputs = Input(shape=(N,))
-    h = Dense(64, activation='sigmoid')(inputs)
+    h = Dense(64, activation='sigmoid', activity_regularizer=activity_l1(lam))(inputs)
     outputs = Dense(N)(h)
 
     model = Model(input=inputs, output=outputs)
+    model.compile(optimizer='adam', loss='mse')
+    model.fit(X, X, batch_size=64, nb_epoch=3)
 
-    def sparse_loss(y_pred, y_true):
-        mse = K.mean(K.square(y_true - y_pred), axis=1)
-        sparse = 1e-3 * K.mean(K.abs(h))
-        return mse + sparse
+    return model, Model(input=inputs, output=h)
 
-    model.compile(optimizer='adam', loss=sparse_loss)
-    model.fit(X, X, batch_size=64, nb_epoch=5)
 
-    return model
+def multilayer_autoencoder(X, lam=1e-5):
+    X = X.reshape(X.shape[0], -1)
+    M, N = X.shape
+
+    inputs = Input(shape=(N,))
+    h = Dense(128, activation='relu')(inputs)
+    encoded = Dense(64, activation='relu', activity_regularizer=activity_l1(lam))(h)
+    h = Dense(128, activation='relu')(encoded)
+    outputs = Dense(N)(h)
+
+    model = Model(input=inputs, output=outputs)
+    model.compile(optimizer='adam', loss='mse')
+    model.fit(X, X, batch_size=64, nb_epoch=3)
+
+    return model, Model(input=inputs, output=h)
 
 
 def conv_autoencoder(X):
@@ -60,14 +74,8 @@ def conv_autoencoder(X):
 
     inputs = Input(shape=(28, 28, 1))
     h = Conv2D(4, 3, 3, activation='relu', border_mode='same')(inputs)
-    h = MaxPooling2D((2, 2))(h)
-    h = Conv2D(8, 3, 3, activation='relu', border_mode='same')(h)
-
-    # 7x7x8 = 392
     encoded = MaxPooling2D((2, 2))(h)
-
-    h = UpSampling2D((2, 2))(encoded)
-    h = Conv2D(8, 3, 3, activation='relu', border_mode='same')(h)
+    h = Conv2D(4, 3, 3, activation='relu', border_mode='same')(encoded)
     h = UpSampling2D((2, 2))(h)
     outputs = Conv2D(1, 3, 3, activation='relu', border_mode='same')(h)
 
@@ -75,7 +83,7 @@ def conv_autoencoder(X):
     model.compile(optimizer='adam', loss='mse')
     model.fit(X, X, batch_size=64, nb_epoch=5)
 
-    return model
+    return model, Model(input=inputs, output=encoded)
 
 
 def contractive_autoencoder(X, lam=1e-3):
@@ -85,8 +93,8 @@ def contractive_autoencoder(X, lam=1e-3):
     N_batch = 100
 
     inputs = Input(shape=(N,))
-    h = Dense(N_hidden, activation='sigmoid', name='encoded')(inputs)
-    outputs = Dense(N, activation='linear')(h)
+    encoded = Dense(N_hidden, activation='sigmoid', name='encoded')(inputs)
+    outputs = Dense(N, activation='linear')(encoded)
 
     model = Model(input=inputs, output=outputs)
 
@@ -95,6 +103,7 @@ def contractive_autoencoder(X, lam=1e-3):
 
         W = K.variable(value=model.get_layer('encoded').get_weights()[0])  # N x N_hidden
         W = K.transpose(W)  # N_hidden x N
+        h = model.get_layer('encoded').output
         dh = h * (1 - h)  # N_batch x N_hidden
 
         # N_batch x N_hidden * N_hidden x 1 = N_batch x 1
@@ -103,13 +112,13 @@ def contractive_autoencoder(X, lam=1e-3):
         return mse + contractive
 
     model.compile(optimizer='adam', loss=contractive_loss)
-    model.fit(X, X, batch_size=N_batch, nb_epoch=5)
+    model.fit(X, X, batch_size=N_batch, nb_epoch=3)
 
-    return model
+    return model, Model(input=inputs, output=encoded)
 
 
 if __name__ == '__main__':
-    model = contractive_autoencoder(X_train)
+    model, representation = contractive_autoencoder(X_train)
 
     idxs = np.random.randint(0, X_test.shape[0], size=5)
     X_recons = model.predict(X_test[idxs])
